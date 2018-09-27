a747e0761cac2a66325e5650111807a0139790c8
josejoab <41337937+josejoab@users.noreply.github.com>
2018-09-26 20:36:46 -0500

    Update TestTaller8.java

--- talleres/taller08/codigo_estudiante/java/TestTaller8.java
+++ talleres/taller08/codigo_estudiante/java/TestTaller8.java
@@ -1,105 +1,95 @@
-package Taller__8;
 
 import java.util.*;
 
-public class TestTaller8 {
-	
-    public static boolean ejercicio1(){
-        Stack<Integer> a,b,c,d;
-        int [] a1,b1,c1,d1,temp1,temp2,temp3,temp4;
-        
-        a = toStack(new int[] {1,2,3,4,5,5});
-        a1 = new int[] {5,5,4,3,2,1};
-        a = Taller8.inversa(a);
-        temp1 = a.stream().mapToInt(Integer::intValue).toArray();
-        
-        b = toStack(new int[] {2,34,3,5,4,6,4,56,5});
-        b1 = new int[] {5,56,4,6,4,5,3,34,2};
-        b = Taller8.inversa(b);
-        temp2 = b.stream().mapToInt(Integer::intValue).toArray();
-        
-        c = toStack(new int[] {33,4,54,2,34,436,5,4,43,2,342,234});
-        c1 = new int[] {234,342,2,43,4,5,436,34,2,54,4,33};
-        c = Taller8.inversa(c);
-        temp3 = c.stream().mapToInt(Integer::intValue).toArray();
-        
-        d = toStack(new int[] {6,5,6,4,3,643,63,23,5,57,56,3,3,5,4});
-        d1 = new int[] {4,5,3,3,56,57,5,23,63,643,3,4,6,5,6};
-        d = Taller8.inversa(d);
-        temp4 = d.stream().mapToInt(Integer::intValue).toArray();
-        
-        if(Arrays.equals(temp1,a1) && Arrays.equals(temp2,b1) && Arrays.equals(temp3,c1) && Arrays.equals(temp4,d1))
-            return true;
-        return false;
-    }
+/**
+ * La clase Taller8 tiene la intención de
+ * enfatizar en el uso de pilas y colas ya 
+ * implementadas en java
+ * @see <a href="https://docs.oracle.com/javase/7/docs/api/java/util/Queue.html"> Ver más acerca de colas<a/>
+ * @see <a href="https://docs.oracle.com/javase/7/docs/api/java/util/Stack.html"> Ver más acerca de pilas <a/> 
+ * @author Mauricio Toro, Andrés Páez 
+ */
+public class Taller8 {
     
-    
-    public static Stack<Integer> toStack(int[]array){
-        Stack<Integer> a=new Stack<Integer>();
-        for(int i=0;i<array.length;i++){
-            a.push(array[i]);
+    /**
+    * @param stack es una pila ya implementada que se crea en el test
+    * Este método se encarga de poner la pila stack en orden inverso
+    * Nota: recuerde que la funcion pop() no solo expulsa la última 
+    * posición de una pila si no que tambien devuelve su valor.
+    * @return una pila que haga el inverso de stack
+    */
+    public static Stack<Integer> inversa (Stack<Integer> stack){
+        Stack<Integer> stack2 = new Stack();
+        while(stack.size()>0){
+            stack2.push(stack.pop());
         }
-        return a;
+        return stack2;
     }
     
     
-    public static void ejercicio2(){
-    	Queue<String> cola1;
-    	String[] a = {"Juan","Maria","Pedro","Mariana"};
-    	cola1=toQueue(a);
-    	Taller8.cola(cola1);
+    /**
+    * @param queue es una cola ya implementada que se crea en el test
+    * Este método se encarga de atender a personas.
+    * Nota: Se debe imprimir en consola a quién atiende y sacarlo de la cola
+    * existe una función "pull" que hace el trabajo más fácil
+    * 
+    */
+    public static void cola (Queue<String> queue){
+        //...
     }
     
-    public static Queue<String> toQueue(String[]array){
-        Queue<String> a=new LinkedList<String>();
-        for(int i=0;i<array.length;i++){
-            a.add(array[i]);
+    
+    /**
+    * @param string es una cadena de texto que viene de este modo 3 4 5 * + o de esta manera 2 3 * 5 +
+    * todo viene separado por espacios por lo que la funcion split.("") vendría muy bien para separarlos
+    * Se intenta hacer el calculo por medio del metodo polaco posfijo
+    * @return un entero resultado de la operación
+    */ 
+    public static int polaca(String string){
+       ArrayList<String> partes = new ArrayList(); 
+       String[] partesA = string.split(" ");
+       for(int i = 0; i < partesA.length; i++){
+         partes.add(partesA[i]);
         }
-        return a;
+       int i = 0;
+       while(partes.size() != 1){
+        if(partes.get(i).equals("+")){
+           String a = ""+(Integer.parseInt(partes.get(0)) + Integer.parseInt(partes.get(1)));
+           partes.remove(0);
+           partes.add(0,a);
+           partes.remove(1);
+           partes.remove("+");
+           i = 0;
+        }
+        else if(partes.get(i).equals("-")){
+           String a = ""+(Integer.parseInt(partes.get(0)) - Integer.parseInt(partes.get(1)));
+           partes.remove(0);
+           partes.add(0,a);
+           partes.remove(1);
+           partes.remove("-");
+           i = 0;
+        }
+        else if(partes.get(i).equals("*")){
+           int a = (Integer.parseInt(partes.get(0)) * Integer.parseInt(partes.get(1)));
+           String b = ""+a;
+           System.out.println("hola" + b);
+           partes.remove(0);
+           partes.add(0,b);
+           partes.remove(1);
+           partes.remove("*");
+           i = 0;
+        }
+        else if(partes.get(i).equals("/")){
+           String a = ""+(Integer.parseInt(partes.get(0)) / Integer.parseInt(partes.get(1)));
+           partes.remove(0);
+           partes.add(0,a);
+           partes.remove(1);
+           partes.remove("/");
+           i = 0;
+        }
+        else i++;
+        }
+        return Integer.parseInt(partes.get(i));
     }
-    
-    
-    public static boolean ejercicio3(){
-    	int a,b,c,d,a1,b1,c1,d1;
-        String s1,s2,s3,s4;
-        
-        a=14;
-        s1 = "3 5 * 12 + 13 -";
-        a1 = Taller8.polaca(s1);
-        
-        b=-172;
-        s2 = "12 5 * 232 -";
-        b1 = Taller8.polaca(s2);
-        
-        c=884;
-        s3 = "34 2 * 13 *";
-        c1 = Taller8.polaca(s3);
-        
-        d=1260;
-        s4 = "67 3 * 21 - 7 *";
-        d1 = Taller8.polaca(s4);
-        
-        if(a==a1 && c==c1 && b==b1 && d==d1)
-            return true;
-        return false;
-    }
-    
-    public static void main(String[] args){
-    	
-        //Ejercicio1
-        if(ejercicio1())
-            System.out.println("Ejercicio 1 Correcto");
-        else
-        System.out.println("Ejercicio 1 Incorrecto");
-        
-        //Ejercicio2
-        ejercicio2();
-        
-        //Ejercicio3
-        if(ejercicio3())
-            System.out.println("Ejercicio 3 Correcto");
-        else
-        System.out.println("Ejercicio 3 Incorrecto");
-        
-    }
+
 }
